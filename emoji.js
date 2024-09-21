8bb6d259f14d1417bf7a5da47f084ff75b611c04
Red2108 <jorge_c_19980@hotmail.com>
2024-09-20 23:17:25 -0500

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -1,12 +1,12 @@
 // script.js
 
-// Seleccionamos el elemento de audio y el emoji
+// Seleccionamos los elementos de audio y el emoji
 const audio = document.getElementById('miAudio');
 const hiddenAudio = document.getElementById('hiddenAudio');
 const lyrics = document.querySelector("#lyrics");
 const playButton = document.getElementById('playAudio');
 
-// Sincronizar las letras con la canción
+// Configuración inicial
 audio.volume = 1.0; // Valor entre 0.0 (silencio) y 1.0 (máximo)
 
 // Agregamos un evento de clic al emoji
@@ -22,25 +22,25 @@ playButton.addEventListener('click', () => {
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-    { text: "Asi que voy a amarte cada noche..", time: 0 },
-    { text: "como si fuera la ultima noche.", time: 2 },
-    { text: "Si el mundo se acabara...", time: 5 },
-    { text: "quisiera estar...", time: 8 },
-    { text: "A tu lado.", time: 10 },
-    { text: "Si la fiesta se terminara...", time: 15 },
-    { text: "y nuestro tiempo en la tierra...", time: 17 },
-    { text: "se acabara.", time: 20 },
-    { text: "Quisiera abrazarte...", time: 24 },
-    { text: "Solo por un momento", time: 26 },
-    { text: "Y morir", time: 28 },
-    { text: "con una sonrisa.", time: 31 },
+    { text: "Asi que voy a amarte cada noche...", time: 0 },
+    { text: "como si fuera la ultima noche.", time: 1.5 },
+    { text: "Si el mundo se acabara...", time: 3 },
+    { text: "quisiera estar...", time: 5.5 },
+    { text: "A tu lado.", time: 7.5 },
+    { text: "Si la fiesta se terminara...", time: 10 },
+    { text: "y nuestro tiempo en la tierra...", time: 12 },
+    { text: "se acabara.", time: 15 },
+    { text: "Quisiera abrazarte...", time: 18 },
+    { text: "Solo por un momento", time: 20 },
+    { text: "Y morir", time: 22 },
+    { text: "con una sonrisa.", time: 24 },
 ];
 
 // Animar las letras
 function updateLyrics() {
     var time = Math.floor(audio.currentTime);
     var currentLine = lyricsData.find(
-        (line) => time >= line.time && time < line.time + 4
+        (line) => time >= line.time && time < line.time + 2
     );
 
     if (currentLine) {
@@ -54,17 +54,17 @@ function updateLyrics() {
     }
 }
 
-// Actualizar las letras cada 500 ms
-setInterval(updateLyrics, 500);
+// Actualizar las letras cada 200 ms
+setInterval(updateLyrics, 200);
 
 // Función para ocultar el título después de 216 segundos
 function ocultarTitulo() {
     var titulo = document.querySelector(".titulo");
-    titulo.style.animation = "fadeOut 0.4s ease-in-out forwards"; // Duración y función de temporización de la desaparición
+    titulo.style.animation = "fadeOut 0.4s ease-in-out forwards"; 
     setTimeout(function () {
         titulo.style.display = "none";
-    }, 500); // Espera antes de ocultar completamente
+    }, 500);
 }
 
-// Llama a la función después de 216 segundos (216,000 milisegundos)
+// Llama a la función después de 216 segundos
 setTimeout(ocultarTitulo, 216000);
